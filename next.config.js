000b1433d572a87c2a4a3ab9726028ae2162f501
Antoine Kingue <etorefr@gmail.com>
2022-08-10 16:03:01 +0200

    🔧 update next config with security headers and compiler usage for styled components

--- next.config.js
+++ next.config.js
@@ -1,16 +1,56 @@
-module.exports = {
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-            use: {
-                loader: 'url-loader',
-                options: {
-                    limit: 100000,
-                    name: '[name].[ext]'
-                }
-            }
-        });
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-Powered-By",
+    value: "Next.js & onRuntime",
+  },
+];
 
-        return config;
-    },
+const config = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 100000,
+          name: "[name].[ext]",
+        },
+      },
+    });
+    return config;
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
+
+module.exports = config;
