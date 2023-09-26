f8ff44249785c6b7bffc803ddc900c7e0fd9826f
Antoine Kingue <contact@antoinek.fr>
2023-09-26 23:58:37 +0200

    🚨 fix what lint errors

--- src/screens/What.tsx
+++ src/screens/What.tsx
@@ -1,5 +1,5 @@
-import PageWrapper from "@components/PageWrapper";
 import Link from "@components/Link";
+import PageWrapper from "@components/PageWrapper";
 import Head from "next/head";
 import calculateAge from "src/utils/calculateAge";
 
