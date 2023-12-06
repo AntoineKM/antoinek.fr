724187cc6bfd8ac44bb22c5801a9690d55268b4c
Antoine Kingue <contact@antoinek.fr>
2023-12-06 14:37:24 +0100

    🔍 update self canonical

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,30 +1,34 @@
 import NextHead from "next/head";
+import { useRouter } from "next/router";
 
-const Head = () => (
-  <NextHead>
-    <meta charSet={"utf-8"} />
-    <link rel={"icon"} href={"/static/images/favicons/favicon.ico"} />
-    <meta name={"viewport"} content={"width=device-width, initial-scale=1"} />
-    <meta name={"theme-color"} content={"#10100e000"} />
-    <meta
-      name={"description"}
-      content={"Antoine Kingue: developer, designer and youtuber"}
-    />
-    <link
-      rel={"apple-touch-icon"}
-      href={"/static/images/favicons/favicon.png"}
-    />
-    <link rel={"canonical"} href={"https://antoinek.fr/"} />
-    {/* add open graph */}
-    <meta property={"og:image"} content={"/static/images/open-graph.jpg"} />
-    {/* add twitter */}
-    <meta name={"twitter:card"} content={"summary_large_image"} />
-    <meta name={"twitter:site"} content={"@AntoineKingue"} />
-    <meta name={"twitter:creator"} content={"@AntoineKingue"} />
-    <meta name={"twitter:image"} content={"/static/images/open-graph.jpg"} />
+const Head = () => {
+  const router = useRouter();
+  return (
+    <NextHead>
+      <meta charSet={"utf-8"} />
+      <link rel={"icon"} href={"/static/images/favicons/favicon.ico"} />
+      <meta name={"viewport"} content={"width=device-width, initial-scale=1"} />
+      <meta name={"theme-color"} content={"#10100e000"} />
+      <meta
+        name={"description"}
+        content={"Antoine Kingue: developer, designer and youtuber"}
+      />
+      <link
+        rel={"apple-touch-icon"}
+        href={"/static/images/favicons/favicon.png"}
+      />
+      <link rel={"canonical"} href={`https://antoinek.fr/${router.asPath}`} />
+      {/* add open graph */}
+      <meta property={"og:image"} content={"/static/images/open-graph.jpg"} />
+      {/* add twitter */}
+      <meta name={"twitter:card"} content={"summary_large_image"} />
+      <meta name={"twitter:site"} content={"@AntoineKingue"} />
+      <meta name={"twitter:creator"} content={"@AntoineKingue"} />
+      <meta name={"twitter:image"} content={"/static/images/open-graph.jpg"} />
 
-    <title>{"Antoine Kingue"}</title>
-  </NextHead>
-);
+      <title>{"Antoine Kingue"}</title>
+    </NextHead>
+  );
+};
 
 export default Head;
