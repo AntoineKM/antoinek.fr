a32f7c29008602fdf0fcb096bd02d401c08ea67a
Antoine Kingue <contact@antoinek.fr>
2023-12-14 16:42:39 +0100

    🎨 add pagination to videos

--- src/screens/Videos.tsx
+++ src/screens/Videos.tsx
@@ -1,17 +1,23 @@
 import PageWrapper from "@components/PageWrapper";
 import Video from "@components/Video";
-import VideoChannel from "@components/VideoChannel";
 import VideoSkeleton from "@components/VideoSkeleton";
 import { NextPage, NextPageContext } from "next";
 import Head from "next/head";
+import Link from "next/link";
 import React from "react";
 import styled from "styled-components";
 
 export type VideosProps = {
   videos: any[];
+  nextPageToken: string;
+  prevPageToken: string;
 };
 
-const VideosPage: NextPage<VideosProps> = ({ videos }: VideosProps) => {
+const VideosPage: NextPage<VideosProps> = ({
+  videos,
+  nextPageToken,
+  prevPageToken,
+}: VideosProps) => {
   return (
     <PageWrapper>
       <Head>
@@ -29,11 +35,21 @@ const VideosPage: NextPage<VideosProps> = ({ videos }: VideosProps) => {
               />
             ))
           : Array.from({ length: 50 }, (_, i) => <VideoSkeleton key={i} />)}
-        <VideoChannel
-          title={"Visit my youtube channel to see more ➡️"}
-          url={"https://youtube.com/@orionmood"}
-        />
       </VideosWrapper>
+      <PaginationContainer>
+        <PaginationButton
+          href={`/videos${prevPageToken ? `?pageToken=${prevPageToken}` : ""}`}
+          disabled={!prevPageToken}
+        >
+          {"⬅️ previous"}
+        </PaginationButton>
+        <PaginationButton
+          href={`/videos${nextPageToken ? `?pageToken=${nextPageToken}` : ""}`}
+          disabled={!nextPageToken}
+        >
+          {"next ➡️"}
+        </PaginationButton>
+      </PaginationContainer>
     </PageWrapper>
   );
 };
@@ -42,7 +58,7 @@ const VideosWrapper = styled.div`
   display: grid;
   width: 100%;
   gap: 2rem 2rem;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
 
   @media (max-width: 1100px) {
     grid-template-columns: 1fr 1fr;
@@ -53,20 +69,57 @@ const VideosWrapper = styled.div`
   }
 `;
 
+const PaginationContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 2rem;
+`;
+
+const PaginationButton = styled(Link)<{ disabled: boolean }>`
+  text-decoration: none;
+  color: inherit;
+  cursor: pointer;
+  border: 1px solid #30302b;
+  border-radius: 10px;
+  padding: 0.5rem 1rem;
+  margin: 0 0.5rem;
+  transition: all 0.1s ease;
+  will-change: transform;
+  transition: background-color 0.2s;
+  font-size: 14px;
+  font-weight: 600;
+
+  &:hover {
+    background-color: #30302b;
+    text-decoration: none !important;
+  }
+
+  ${({ disabled }) =>
+    disabled &&
+    `
+    pointer-events: none;
+    opacity: 0.5;
+  `}
+`;
+
 VideosPage.getInitialProps = async (ctx: NextPageContext) => {
+  const { query } = ctx;
+
+  const pageToken = (query.pageToken as string) || "";
   const playListID = "UUN0hmDGaj1RAshd3A-x35pA";
   const mykey = process.env.NEXT_PUBLIC_YOUTUBE_KEY || "";
-  const URL = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=14&playlistId=${playListID}&key=${mykey}`;
+  const maxResults = 12;
+  const URL = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=${maxResults}&playlistId=${playListID}&key=${mykey}&pageToken=${pageToken}`;
 
   try {
     const response = await fetch(URL);
     const data = await response.json();
-    const videos = data.items;
+    const { nextPageToken, prevPageToken, items: videos } = data;
 
-    return { videos };
+    return { videos, nextPageToken, prevPageToken };
   } catch (error) {
     console.error("Error fetching data:", error);
-    return { videos: [] };
+    return { videos: [], nextPageToken: "", prevPageToken: "" };
   }
 };
 
