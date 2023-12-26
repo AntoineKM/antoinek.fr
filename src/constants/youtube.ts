3c432c68e9bc858204ed3e8017958f6b1a30519b
Antoine Kingue <contact@antoinek.fr>
2023-12-26 11:44:59 +0100

    🔧 update youtube playlist id

--- src/constants/youtube.ts
+++ src/constants/youtube.ts
@@ -1,4 +1,4 @@
 export const YOUTUBE = {
   CHANNEL_ID: "UCN0hmDGaj1RAshd3A-x35pA",
-  PLAYLIST_ID: "UCN0hmDGaj1RAshd3A-x35pA",
+  PLAYLIST_ID: "UUN0hmDGaj1RAshd3A-x35pA",
 };
--- src/pages/api/youtube/playlistItems.ts
+++ src/pages/api/youtube/playlistItems.ts
@@ -1,5 +1,6 @@
 import { env } from "env.mjs";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { YOUTUBE } from "src/constants/youtube";
 import urlcat from "urlcat";
 
 type ResponseData = any;
@@ -8,11 +9,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>,
 ) {
-  const playlistId = "UUN0hmDGaj1RAshd3A-x35pA";
   const enpoint = "https://www.googleapis.com/youtube/v3/playlistItems";
   const url = urlcat(enpoint, {
     ...req.query,
-    playlistId,
+    playlistId: YOUTUBE.PLAYLIST_ID,
     key: env.YOUTUBE_API_KEY,
     part: req.query.part || "snippet",
     maxResults: req.query.maxResults || 12,
