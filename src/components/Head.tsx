3f6437a9d3d77165195f2f0496b05361bea7c5bc
Antoine Kingue <contact@antoinek.fr>
2024-05-19 10:20:18 +0200

    :mag: update canonical by including pagetoken

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -3,12 +3,22 @@ import { useRouter } from "next/router";
 
 const Head = () => {
   const router = useRouter();
+  const url = new URL(`https://antoinek.fr${router.asPath}`);
+  const params = new URLSearchParams(url.search);
+  const pageToken = params.get("pageToken");
+
+  if (pageToken) {
+    url.search = `?pageToken=${pageToken}`;
+  } else {
+    url.search = "";
+  }
+
   return (
     <NextHead>
       <meta charSet={"utf-8"} />
       <link rel={"icon"} href={"/static/images/favicons/favicon.ico"} />
       <meta name={"viewport"} content={"width=device-width, initial-scale=1"} />
-      <meta name={"theme-color"} content={"#10100e000"} />
+      <meta name={"theme-color"} content={"#10100e"} />
       <meta
         name={"description"}
         content={"Antoine Kingue: developer, designer and youtuber"}
@@ -17,10 +27,7 @@ const Head = () => {
         rel={"apple-touch-icon"}
         href={"/static/images/favicons/favicon.png"}
       />
-      <link
-        rel={"canonical"}
-        href={`https://antoinek.fr${router.asPath.split("?")[0]}`}
-      />
+      <link rel={"canonical"} href={url.href} />
       {/* add open graph */}
       <meta property={"og:image"} content={"/static/images/open-graph.jpg"} />
       {/* add twitter */}
