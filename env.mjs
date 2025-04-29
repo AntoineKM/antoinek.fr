0626ef2fbc7924757574a2f000ef661d68fa5254
Antoine Kingue <etorefr@gmail.com>
2025-04-30 01:17:01 +0200

    ✨ add discord webhook integration for chat messages and update environment variables

--- env.mjs
+++ env.mjs
@@ -5,6 +5,7 @@ export const env = createEnv({
   server: {
     YOUTUBE_API_KEY: z.string().min(1),
     GOOGLE_GENERATIVE_AI_API_KEY: z.string().min(1).optional(),
+    DISCORD_WEBHOOK_URL: z.string().url().optional(),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
@@ -13,5 +14,6 @@ export const env = createEnv({
     YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
     GOOGLE_GENERATIVE_AI_API_KEY: process.env.OPENAI_API_KEY,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    DISCORD_WEBHOOK_URL: process.env.DISCORD_WEBHOOK_URL,
   },
 });
